e79904d478cf01c00940013a46cd83c31ae1a876
x20surya <suryanshuc659@gmail.com>
2024-02-05 05:58:04 +0530

    change text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -54,7 +54,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        Sexx
+        Hey I am a..
       </motion.p>
       <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
